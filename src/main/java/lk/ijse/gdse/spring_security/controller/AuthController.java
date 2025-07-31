f6dd5c236bbba41f09c73390cefa0233c390fab7
ErangaFernando404 <erangafdo1994@gmail.com>
2025-07-31 15:00:49 +0530

    create AuthController

---
+++ src/main/java/lk/ijse/gdse/spring_security/controller/AuthController.java
@@ -0,0 +1,41 @@
+package lk.ijse.gdse.spring_security.controller;
+
+import lk.ijse.gdse.spring_security.dto.ApiResponse;
+import lk.ijse.gdse.spring_security.dto.AuthDto;
+import lk.ijse.gdse.spring_security.dto.RegisterDto;
+import lk.ijse.gdse.spring_security.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<ApiResponse> registerUser(@RequestBody RegisterDto registerDTO) {
+        return ResponseEntity.ok(
+                new ApiResponse(
+                        200,
+                        "User Register Successfully",
+                        authService.register(registerDTO)
+                )
+        );
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<ApiResponse> login(@RequestBody AuthDto authDTO) {
+        return ResponseEntity.ok(
+                new ApiResponse(
+                        200,
+                        "OK",
+                        authService.authenticate(authDTO)
+                )
+        );
+    }
+}
