cb662b3e3a4dac0eb60d35e78e93dc0da3c41a68
ErangaFernando404 <erangafdo1994@gmail.com>
2025-07-31 11:35:08 +0530

    create UserRepository

---
+++ src/main/java/lk/ijse/gdse/spring_security/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse.spring_security.repository;
+
+
+import lk.ijse.gdse.spring_security.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+     Optional<User> findByUsername(String username);
+}
