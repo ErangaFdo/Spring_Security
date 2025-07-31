c941645479af1586cb454b12a5dfb5db599cff6b
ErangaFernando404 <erangafdo1994@gmail.com>
2025-07-31 14:11:19 +0530

    create Config

---
+++ src/main/java/lk/ijse/gdse/spring_security/config/ApplicationConfig.java
@@ -0,0 +1,37 @@
+package lk.ijse.gdse.spring_security.config;
+
+
+import lk.ijse.gdse.spring_security.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository.findByUsername(username)
+                .map(user -> new User(
+                        user.getUserName(),
+                        user.getUserPassword(),
+                        List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()))
+                )).orElseThrow(
+                        () -> new RuntimeException("User not found")
+                );
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
