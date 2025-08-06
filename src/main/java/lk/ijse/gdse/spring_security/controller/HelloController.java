a20637f2f065c418b24969c04ed9b19e9c0c9f5b
ErangaFernando404 <erangafdo1994@gmail.com>
2025-08-06 08:06:38 +0530

    check role (helloController)

---
+++ src/main/java/lk/ijse/gdse/spring_security/controller/HelloController.java
@@ -0,0 +1,23 @@
+package lk.ijse.gdse.spring_security.controller;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("hello")
+public class HelloController {
+
+    @GetMapping("get")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String helloAdmin(){
+        return "Hello Admin";
+    }
+
+    @GetMapping("user")
+    @PreAuthorize("hasRole('USER')")
+    public String helloUser(){
+        return "Hello User";
+    }
+}
