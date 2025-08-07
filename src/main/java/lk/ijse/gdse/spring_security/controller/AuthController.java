fc60ac67f007ecf82e585a994a27feb94dd62d66
ErangaFernando404 <erangafdo1994@gmail.com>
2025-08-07 22:23:44 +0530

    update AuthController

--- src/main/java/lk/ijse/gdse/spring_security/controller/AuthController.java
+++ src/main/java/lk/ijse/gdse/spring_security/controller/AuthController.java
@@ -6,14 +6,12 @@ import lk.ijse.gdse.spring_security.dto.RegisterDto;
 import lk.ijse.gdse.spring_security.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class AuthController {
     private final AuthService authService;
 
